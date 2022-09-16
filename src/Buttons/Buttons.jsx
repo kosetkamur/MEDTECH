7e468fe6ef3caa2f5b39ed88e709e86736dfca0a
kosetkamur <nataliyamaxi2902@gmail.com>
2022-09-16 14:36:01 +0300

    update

--- src/Buttons/Buttons.jsx
+++ src/Buttons/Buttons.jsx
@@ -5,20 +5,23 @@ import '../App.css';
 
 const Buttons = ({numberWeek,items,index}) => {
 
-    const [weeks, setWeeks] = useState([])
+    const [weeks, setWeeks] = useState([]);
+    const [close, setClose] = useState('close');
 
     const buttonClick = (num) =>{
+        setClose('close active')
         return setWeeks(weeks.concat(items.week));
     }
 
     const closeRasp = () =>{
+        setClose('close')
         return setWeeks([]);
     }
     return (
         <div className="blockWeek">
             <button onClick={()=>buttonClick({numberWeek})} id={index}>Неделя {numberWeek}</button>
             <div className="week">{weeks.map((week) => <Block weekday={week.weekday} lessons={week.lesson} />)}</div>
-            <button onClick={()=>closeRasp()}>Закрыть</button>
+            <button onClick={()=>closeRasp()} className={close}>Закрыть</button>
         </div>
     );
 };
--- src/Lessons/Lessons.jsx
+++ src/Lessons/Lessons.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Lessons.css'
 
 const Lessons = ({name, time, classroom}) => {
