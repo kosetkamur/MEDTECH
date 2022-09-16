a97dfa8aca1e7d9b50069d5bdb13d1667b48766f
kosetkamur <nataliyamaxi2902@gmail.com>
2022-09-16 14:55:07 +0300

    updateDB

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Buttons from "./Buttons/Buttons";
 function App() {
   return (
     <div className="App">
+      <h1>Группа 209</h1>
       {data.map((item,index) => <Buttons numberWeek={item.numberWeek} items={item} key={index}/>)}
     </div>
   );
